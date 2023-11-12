dbd89f5922eb1a7b825b37d2af294a37e944a9f2
dallensmith1@mail.weber.edu <Dallen Smith>
2023-11-12 16:55:03 -0700

    changed a bit more

--- expenses/views.py
+++ expenses/views.py
@@ -7,5 +7,5 @@ from .models import Transaction
 
 def expense_list(request):
     expenses = Transaction.objects.all()
-    return render(request, 'expenses/expense_list.html', {'expenses': expenses})
+    return render(request, 'budget/category.html', {'expenses': expenses})
 
